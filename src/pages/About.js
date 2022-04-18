7dcc550e0a86bdbc44a37e7f812fc26eda4b94c9
ekrem0691 <06hilalekrem06@gmail.com>
2022-04-19 02:44:59 +0300

    changed

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,35 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About Sayfası</div>
+
+
+    <div>About Sayfası
+
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+
+      <h3 align="center"> FrontEnd | FullStack </h3>
+
+
+
+
+
+
+      <p align="center">
+        <a href="https://twitter.com/beyyy_kerem" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/twitter.svg" alt="mrakhilg" height="30" width="30" /></a>
+        <a href="https://www.linkedin.com/in/ekrm-sryr-444761223/" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg" alt="akhilgkrishnan" height="30" width="30" /></a>
+        <a href="https://stackoverflow.com/users/18004535/ekrm-sryr" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/stackoverflow.svg" alt="10321356" height="30" width="30" /></a>
+        <a href="https://www.facebook.com/kerembey" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/facebook.svg" alt="akhilgkrishnan9800" height="30" width="30" /></a>
+        <a href="https://instagram.com/keremsryr" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/instagram.svg" alt="akhilgkrishnan" height="30" width="30" /></a>
+        <a href="https://medium.com/@06hilalekrem06" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/medium.svg" alt="@akhilgkrishnan" height="30" width="30" /></a>
+      </p>
+
+    </div>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home Sayfası</div>
+    <div>Home Sayfası
+
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <p> ✔ Callme: ***He/His*** or ***TeamWorker*** 😊</p>
+      <p> ✔ I’m currently Developing Frontend in FullStack **React library**</p>
+      <p>✔ I’m currently learning about **development learning**🥰</p>
+      <p>✔ I’m looking to collaborate with any **Open - Source FrontEnd projects**</p>
+      <p>✔ Ask from me anything you want, If I am alive I will answer within seconds 😉</p>
+      <p>✔ Fun fact : *I Always try to learn something new and then sleep till it store in the brain* 😎</p>
+
+
+    </div>
   )
 }
 
